bf39df62b085f3763469b27b0a8cc014f0fb1e17
gzy <1522780701@qq.com>
2018-01-22 17:46:17 +0800

    map2后台代码1

--- map2/map2.py
+++ map2/map2.py
@@ -13,8 +13,9 @@ import Tool.ExcelTool as ExcelTool
 # 获得table数据 json格式,
 # 对每个excel的数据，进行排序，找出每个省份出口前5和进口前5
 def getTableData():
-    countryNum = 5  # 每个省份的前5个进口货出口。这个参数可以从页面上传进来
-    provinceInfo = ExcelTool.getArrayBySheetName("\\country_excel\\Province.xlsx","province")  #获取省份名列表，包括 ： 中文名、英文名、纬度、经度
+    proShowNum = 5  # 每个省份的前5个进口货出口。默认是5，这个参数可以从页面上传进来
+    provinceNum=31  #省份数量，默认是31
+    provincesInfo = ExcelTool.getArrayBySheetName("\\country_excel\\Province.xlsx","province")  #获取省份名列表，包括 ： 中文名、英文名、纬度、经度
     print(os.getcwd())
     #files = get_file_name_list()
     files = ExcelTool.listExcelFile("\\excel_map2")
@@ -26,8 +27,8 @@ def getTableData():
         try :
             result = {}  # 单个excel文件处理后的结果
             file_name = file.split("\\")[len(file.split("\\")) - 1].split(".")[0]
-            result['importProvinceNum']=countryNum
-            result['exportProvinceNum']=countryNum
+            result['importProvinceNum']=proShowNum
+            result['exportProvinceNum']=proShowNum
             result["fileName"] = file_name
             print file + " start"
             excelData = xlrd.open_workbook(file, "rb")
@@ -48,13 +49,35 @@ def getTableData():
                 Tot[i, len(Tot[0]) - 1] = 0
                 Tot[len(Tot[0]) - 1, i] = 0
 
+            Tot_importSort = np.argsort(-Tot,axis=0)  #按列排序, 进口排序，降序
+            Tot_exportSort = np.argsort(-Tot,axis=1)  #按行排序，出口排序，降序
+            #对31个省份循环处理
+            for i in range(provinceNum):
+                proInfo = provincesInfo[i]
+
+                provinceResult=[]
+                exportData=
+
+                proInfo["exportData"]
+                proInfo["importData"]
+
+                result[]=
 
 
+
+
+
+            resultList.append(result)
+            #一个文件计算完毕
         except BaseException:
             print "Error: 文件有问题," + file
             errMsg += file + "<br/>"
 
-    return
+    resultListJson=json.dumps(resultList)
+    print "返回值 resultListJson :"
+    print   resultListJson
+    return  resultListJson
+
 
 
 
